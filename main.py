ed50be9dc6e279193a8ca636f4232ec2edafd49b
abrahul100 <abrahul100@gmail.com>
2025-03-25 14:30:40 +0530

    Added a DrawingScene class with functionality to draw rectangles on mouse click

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QGraphicsView, QGraphicsScene
+from PyQt5.QtWidgets import QApplication, QMainWindow, QGraphicsView, QGraphicsScene, QGraphicsRectItem, QGraphicsEllipseItem
+from PyQt5.QtCore import Qt
+
+class DrawingScene(QGraphicsScene):
+    def __init__(self):
+        super().__init__()
+        self.mode = "rectangle"  # Default mode to draw rectangles
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            pos = event.scenePos()
+            if self.mode == "rectangle":
+                rect = QGraphicsRectItem(pos.x(), pos.y(), 100, 50)
+                rect.setBrush(Qt.yellow)
+                self.addItem(rect)
+            elif self.mode == "ellipse":
+                ellipse = QGraphicsEllipseItem(pos.x(), pos.y(), 100, 50)
+                ellipse.setBrush(Qt.green)
+                self.addItem(ellipse)
+        super().mousePressEvent(event)
 
 class SimpleDrawingApp(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Basic Drawing Tool")
         self.setGeometry(100, 100, 800, 600)
-
-        self.scene = QGraphicsScene()
+        self.scene = DrawingScene()
         self.view = QGraphicsView(self.scene)
         self.setCentralWidget(self.view)
 
